bc1a74ce5de2882570c62563200f87fb99b15a96
Alexandre <alexandre.omph@gmail.com>
2020-09-19 05:54:33 -0300

    feat: reset password

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,7 @@
+
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
@@ -34,6 +36,34 @@ class ForgotPasswordController {
       return response
         .status(err.status)
         .send({ error: { message: 'Algo deu errado' } })
+    }
+  }
+
+  async update({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response
+          .status(401)
+          .send({ error: { message: 'O token de recuperação inspirado.' } })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Token inválido' } })
     }
   }
 }
